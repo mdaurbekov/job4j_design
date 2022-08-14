0ae43c764811a7d013faeb4bc03a6cb6ae1795b2
mdaurbekov <dreamkov@gmail.com>
2022-08-14 16:09:29 +0300

    upload files

--- src/main/java/ru/job4j/App.java
+++ src/main/java/ru/job4j/App.java
@@ -2,13 +2,10 @@ package ru.job4j;
 
 /**
  * Hello world!
- *
  */
-public class App
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
 
     }
 
