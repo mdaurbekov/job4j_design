fc43f31fe5b953822a04fb9d58e20b28b68aced1
mdaurbekov <dreamkov@gmail.com>
2022-08-17 10:24:45 +0300

    hw 2.1.1. AssertJ #504885

--- src/main/java/ru/job4j/SimpleConvert.java
+++ src/main/java/ru/job4j/SimpleConvert.java
@@ -1,6 +1,9 @@
 package ru.job4j;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
--- src/test/java/ru/job4j/SimpleConvertTest.java
+++ src/test/java/ru/job4j/SimpleConvertTest.java
@@ -7,7 +7,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class SimpleConvertTest {
     @Test
@@ -20,6 +20,7 @@ class SimpleConvertTest {
                 .containsAnyOf("zero", "second", "six")
                 .doesNotContain("first", Index.atIndex(1));
     }
+
     @Test
     void checkList() {
         SimpleConvert simpleConvert = new SimpleConvert();
