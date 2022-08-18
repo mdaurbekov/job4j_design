22e48d8f75e8a50022df36aaaa7d9b72d3c5a105
mdaurbekov <dreamkov@gmail.com>
2022-08-18 09:30:51 +0300

    2.1.2. Iterator even

---
+++ src/main/java/ru/job4j/EvenNumbersIterator.java
@@ -0,0 +1,35 @@
+package ru.job4j;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class EvenNumbersIterator implements Iterator<Integer> {
+
+    private int[] data;
+    private int index = 0;
+
+    public EvenNumbersIterator(int[] data) {
+        this.data = data;
+    }
+
+    @Override
+    public boolean hasNext() {
+
+        for (int i = index; i < data.length; i++) {
+            if ((data[i] & 1) == 0) {
+                index = i;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public Integer next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        return data[index++];
+    }
+
+}
