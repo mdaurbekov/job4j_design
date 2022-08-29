6e78f2cd9a182a2caa6521e296fa37b2a9d87094
mdaurbekov <dreamkov@gmail.com>
2022-08-29 16:11:21 +0300

    Tree

--- src/main/java/ru/job4j/tree/SimpleTree.java
+++ src/main/java/ru/job4j/tree/SimpleTree.java
@@ -3,6 +3,7 @@ package ru.job4j.tree;
 import java.util.LinkedList;
 import java.util.Optional;
 import java.util.Queue;
+import java.util.function.Predicate;
 
 public class SimpleTree<E> implements Tree<E> {
     private final Node<E> root;
@@ -14,8 +15,9 @@ public class SimpleTree<E> implements Tree<E> {
     @Override
     public boolean add(E parent, E child) {
         boolean rsl = false;
-        if (findBy(parent).isPresent()) {
-            Node<E> n = findBy(parent).get();
+        Optional<Node<E>> p = findBy(parent);
+        if (p.isPresent()) {
+            Node<E> n = p.get();
             if (findBy(child).isEmpty()) {
                 rsl = n.children.add(new Node<>(child));
             }
@@ -25,9 +27,23 @@ public class SimpleTree<E> implements Tree<E> {
     }
 
     public boolean isBinary() {
+      //  findBy()
         return false;
     }
-
+    private Optional<Node<E>> findByPredicate(Predicate<Node<E>> condition) {
+        Optional<Node<E>> rsl = Optional.empty();
+//        Queue<Node<E>> data = new LinkedList<>();
+//        data.offer(this.root);
+//        while (!data.isEmpty()) {
+//            Node<E> el = data.poll();
+//            if (el.value.equals()) {
+//                rsl = Optional.of(el);
+//                break;
+//            }
+//            data.addAll(el.children);
+//        }
+        return rsl;
+    }
     @Override
     public Optional<Node<E>> findBy(E value) {
         Optional<Node<E>> rsl = Optional.empty();
