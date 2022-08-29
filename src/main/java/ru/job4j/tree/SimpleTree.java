7df935122d25ef53252c82f8916094f674949cae
mdaurbekov <dreamkov@gmail.com>
2022-08-29 16:12:04 +0300

    Tree

--- src/main/java/ru/job4j/tree/SimpleTree.java
+++ src/main/java/ru/job4j/tree/SimpleTree.java
@@ -27,23 +27,27 @@ public class SimpleTree<E> implements Tree<E> {
     }
 
     public boolean isBinary() {
-      //  findBy()
+
         return false;
     }
+
     private Optional<Node<E>> findByPredicate(Predicate<Node<E>> condition) {
         Optional<Node<E>> rsl = Optional.empty();
-//        Queue<Node<E>> data = new LinkedList<>();
-//        data.offer(this.root);
-//        while (!data.isEmpty()) {
-//            Node<E> el = data.poll();
-//            if (el.value.equals()) {
-//                rsl = Optional.of(el);
-//                break;
-//            }
-//            data.addAll(el.children);
-//        }
+        /*
+        Queue<Node<E>> data = new LinkedList<>();
+        data.offer(this.root);
+        while (!data.isEmpty()) {
+            Node<E> el = data.poll();
+            if (el.value.equals()) {
+                rsl = Optional.of(el);
+                break;
+            }
+            data.addAll(el.children);
+        }
+        */
         return rsl;
     }
+
     @Override
     public Optional<Node<E>> findBy(E value) {
         Optional<Node<E>> rsl = Optional.empty();
