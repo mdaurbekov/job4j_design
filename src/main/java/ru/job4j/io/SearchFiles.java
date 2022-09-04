fa34909a2c998facfaf15d9ff0b4499960dce5ad
mdaurbekov <dreamkov@gmail.com>
2022-09-04 20:11:59 +0300

    io SearchFile

---
+++ src/main/java/ru/job4j/io/SearchFiles.java
@@ -0,0 +1,35 @@
+package ru.job4j.io;
+
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+import static java.nio.file.FileVisitResult.CONTINUE;
+
+public class SearchFiles extends SimpleFileVisitor<Path> {
+
+    List<Path> list = new ArrayList<>();
+    Predicate<Path> condition;
+
+    public SearchFiles(Predicate<Path> condition) {
+        this.condition = condition;
+    }
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        if (condition.test(file.getFileName())) {
+            list.add(file);
+        }
+        return CONTINUE;
+    }
+
+
+    public List<Path> getPaths() {
+        return list;
+    }
+}
