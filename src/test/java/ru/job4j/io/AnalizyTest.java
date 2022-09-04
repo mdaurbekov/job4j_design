3ecc5510f66878ee37d587406398b0d49b380343
mdaurbekov <dreamkov@gmail.com>
2022-09-04 10:43:17 +0300

    io analizytest

--- src/test/java/ru/job4j/io/AnalizyTest.java
+++ src/test/java/ru/job4j/io/AnalizyTest.java
@@ -1,32 +1,35 @@
 package ru.job4j.io;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 
 import java.io.*;
+import java.nio.file.Path;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class AnalizyTest {
 
     @Test
-    public void checkTheServer() throws IOException {
-        File source = new File("log.txt");
-        File target = new File("unavailable.csv");
+    public void checkTheServer(@TempDir Path tempDir) throws IOException {
+        File source = tempDir.resolve("log1.txt").toFile();
+        File target = tempDir.resolve("unavailable1.csv").toFile();
         try (PrintWriter out = new PrintWriter(source)) {
             out.println("200 10:56:01");
             out.println("500 10:57:01");
             out.println("400 10:58:01");
-            out.println("200 10:59:01;");
+            out.println("200 10:59:01");
             out.println("500 11:01:02");
             out.println("200 11:02:02");
 
         }
         Analizy firstTest = new Analizy();
-        firstTest.unavailable("log.txt", "unavailable.csv");
+        firstTest.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
         StringBuilder rsl = new StringBuilder();
         try (BufferedReader readerIn = new BufferedReader(new FileReader(target))) {
             readerIn.lines().forEach(rsl::append);
         }
-        assertThat("10:57:01;10:59:01;11:01:02;11:02:02").isEqualTo(rsl.toString());
+        assertThat("10:57:01;10:59:0111:01:02;11:02:02").isEqualTo(rsl.toString());
+
     }
 }
