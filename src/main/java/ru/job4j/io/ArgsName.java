ca913da639cab55e34634d6112453c55df3b99cb
mdaurbekov <dreamkov@gmail.com>
2022-09-07 16:41:07 +0300

    io validation

--- src/main/java/ru/job4j/io/ArgsName.java
+++ src/main/java/ru/job4j/io/ArgsName.java
@@ -14,21 +14,34 @@ public class ArgsName {
         return values.get(key);
     }
 
-    private void parse(String[] args) {
-        if (args.length < 1) {
-            throw new IllegalArgumentException();
+    private boolean checkParameters(String str) {
+        if (!str.contains("=")) {
+            throw new IllegalArgumentException("Отсутствует символ =");
         }
+        String[] strings = str.replaceFirst("-", "").split("=", 2);
+
+        if (strings[0].isBlank()) {
+            throw new IllegalArgumentException("Отсутствует ключ");
+        }
+        if (strings[1].isBlank()) {
+            throw new IllegalArgumentException("Отсутствует значение");
+        }
+        return true;
+    }
+
+    private void parse(String[] args) {
         for (String str : args) {
+            checkParameters(str);
             String[] strings = str.replaceFirst("-", "").split("=", 2);
-            if (strings.length < 2 || strings[0].isBlank() || strings[1].isBlank()) {
-                throw new IllegalArgumentException();
-            }
             values.put(strings[0], strings[1]);
         }
         System.out.println();
     }
 
     public static ArgsName of(String[] args) {
+        if (args.length < 1) {
+            throw new IllegalArgumentException();
+        }
         ArgsName names = new ArgsName();
         names.parse(args);
         return names;
--- src/test/java/ru/job4j/io/ArgsNameTest.java
+++ src/test/java/ru/job4j/io/ArgsNameTest.java
@@ -1,30 +1,32 @@
 package ru.job4j.io;
 
 import org.junit.jupiter.api.Test;
-import static org.assertj.core.api.Assertions.*;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ArgsNameTest {
     @Test
     void whenGetFirst() {
-        ArgsName jvm = ArgsName.of(new String[] {"-Xmx=512", "-encoding=UTF-8"});
+        ArgsName jvm = ArgsName.of(new String[]{"-Xmx=512", "-encoding=UTF-8"});
         assertThat(jvm.get("Xmx")).isEqualTo("512");
     }
 
     @Test
     void whenGetFirstReorder() {
-        ArgsName jvm = ArgsName.of(new String[] {"-encoding=UTF-8", "-Xmx=512"});
+        ArgsName jvm = ArgsName.of(new String[]{"-encoding=UTF-8", "-Xmx=512"});
         assertThat(jvm.get("Xmx")).isEqualTo("512");
     }
 
     @Test
     void whenMultipleEqualsSymbol() {
-        ArgsName jvm = ArgsName.of(new String[] {"-request=?msg=Exit="});
+        ArgsName jvm = ArgsName.of(new String[]{"-request=?msg=Exit="});
         assertThat(jvm.get("request")).isEqualTo("?msg=Exit=");
     }
 
     @Test
     void whenGetNotExist() {
-        ArgsName jvm = ArgsName.of(new String[] {"-Xmx=512"});
+        ArgsName jvm = ArgsName.of(new String[]{"-Xmx=512"});
         assertThatThrownBy(() -> jvm.get("Xms")).isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -32,6 +34,24 @@ class ArgsNameTest {
     void whenWrongSomeArgument() {
         assertThatThrownBy(() -> ArgsName.of(new String[]{}))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void whenNotValue() {
+        assertThatThrownBy(() -> ArgsName.of(new String[]{"-request="}))
+                .isInstanceOf(IllegalArgumentException.class).hasMessage("Отсутствует значение");
+    }
+
+    @Test
+    void whenNotSymbol() {
+        assertThatThrownBy(() -> ArgsName.of(new String[]{"-request:egerg"}))
+                .isInstanceOf(IllegalArgumentException.class).hasMessage("Отсутствует символ =");
+    }
+
+    @Test
+    void whenNotKey() {
+        assertThatThrownBy(() -> ArgsName.of(new String[]{"=value"}))
+                .isInstanceOf(IllegalArgumentException.class).hasMessage("Отсутствует ключ");
     }
 
 }
