a89f87222f4bad3ed89cb94c3fb3f38bbb3f82f7
mdaurbekov <dreamkov@gmail.com>
2022-09-09 20:44:11 +0300

    io zip

--- src/main/java/ru/job4j/io/Zip.java
+++ src/main/java/ru/job4j/io/Zip.java
@@ -36,14 +36,35 @@ public class Zip {
         }
     }
 
+    public static void checkArgZip(String[] strings) {
+        ArgsName.of(strings);
+        for (String str : strings) {
+            String[] pair = str.replaceFirst("-", "").split("=", 2);
+            String key = pair[0];
+            String value = pair[1];
+            if (key.equals("d")) {
+                Path path = Paths.get(value);
+                if (!Files.isDirectory(path)) {
+                    throw new IllegalArgumentException("Неверный формат директории");
+                }
+
+            }
+            if (key.equals("e") & !value.matches("\\.[^\\.]+")) {
+                throw new IllegalArgumentException("Неверное расширение");
+            }
+            if (key.equals("o") & !value.matches("(.*).zip")) {
+                throw new IllegalArgumentException("Неверный формат архива");
+            }
+
+        }
+    }
 
     public static void main(String[] args) throws IOException {
         if (args.length != 3) {
             throw new IllegalArgumentException("Неверное количество параметров");
         }
+        checkArgZip(args);
         Zip zip1 = new Zip();
-        ArgsName argsName = new ArgsName();
-        argsName.checkArgZip(args);
         Path path = Paths.get(ArgsName.of(args).get("d"));
         SearchFiles searchFiles = new SearchFiles(p -> !p.toFile().getName().endsWith(ArgsName.of(args).get("e")));
         Files.walkFileTree(path, searchFiles);
