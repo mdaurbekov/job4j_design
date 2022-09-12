9729b386ae5077e7d1850943a2116e99fd06b13c
mdaurbekov <dreamkov@gmail.com>
2022-09-12 13:56:22 +0300

    slf4j

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -8,10 +8,15 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        LOG.trace("trace message");
-        LOG.debug("debug message");
-        LOG.info("info message");
-        LOG.warn("warn message");
-        LOG.error("error message");
+        char ch = 'P';
+        int in = 33;
+        double doubl = 3.4;
+        float fl = 4.3f;
+        boolean bool = true;
+        byte bt = 2;
+        short sh = 7;
+        long lg = 5L;
+
+        LOG.debug("Char: {}, int: {}, double: {}, float: {}, boolean: {}, byte: {}, short: {}, long: {}", ch, in, doubl, fl, bool, bt, sh, lg);
     }
 }
