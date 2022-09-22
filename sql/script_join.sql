25ca66fff50642fe73ab73a0790d24b8cbeb6510
mdaurbekov <dreamkov@gmail.com>
2022-09-22 17:58:20 +0300

    sql

--- sql/script_join.sql
+++ sql/script_join.sql
@@ -1,43 +1,46 @@
-create table car
-(
-    id   serial primary key,
-    name varchar(30),
-    year date
-);
-
 create table person
 (
     id     serial primary key,
     name   varchar(30),
-    age    int,
-    car_id int references car (id)
+    age    int
 );
 
-insert into car(name, year)
-values ('Ford', date '1997-01-01'),
-       ('Nissan', date '1995-01-01'),
-       ('BMW', date '1990-01-01'),
-       ('Mazda', date '2000-01-01'),
-       ('Mercedes', date '1999-01-01');
+
+create table car
+(
+    id   serial primary key,
+    name varchar(30),
+    year date,
+    person_id int references person(id)
+);
+
+insert into person(name, age)
+values ('Ivan', 35),
+       ('Maksim', 38),
+       ('Oleg', 34),
+       ('Kirill', 30),
+       ('Stepan', 46);
+
+insert into car(name, year, person_id)
+values ('Ford', date '1997-01-01', 1),
+       ('Nissan', date '1995-01-01', 1),
+       ('BMW', date '1990-01-01', 4),
+       ('Mazda', date '2000-01-01', 3),
+       ('Mercedes', date '1999-01-01', 2);
 
 
-insert into person(name, age, car_id)
-values ('Ivan', 35, 1),
-       ('Maksim', 38, 1),
-       ('Oleg', 34, 3),
-       ('Kirill', 30, 1),
-       ('Stepan', 46, 2);
+
 
 select p.name, c.name, to_char(c.year:: date, 'YYYY')
 from person as p
-         join car c on c.id = p.car_id;
+         join car c on c.person_id = p.id;
 
 select p.name
 from person as p
-         join car c on c.id = p.car_id
+         join car c on c.person_id = p.id
 where c.name = 'Ford';
 
 select c.name
 from person as p
-         inner join car c on c.id = p.car_id
+         inner join car c on c.person_id = p.id
 where p.age = 34;
