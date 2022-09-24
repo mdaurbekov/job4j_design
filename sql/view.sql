12be4d2ddcce4f98a87569f14ff0151d54cfdb29
mdaurbekov <dreamkov@gmail.com>
2022-09-24 23:51:32 +0300

    sql

---
+++ sql/view.sql
@@ -0,0 +1,49 @@
+create table students (
+                          id serial primary key,
+                          name varchar(50)
+);
+
+insert into students (name) values ('Иван Иванов');
+insert into students (name) values ('Петр Петров');
+create table authors (
+                         id serial primary key,
+                         name varchar(50)
+);
+
+insert into authors (name) values ('Александр Пушкин');
+insert into authors (name) values ('Николай Гоголь');
+
+create table books (
+                       id serial primary key,
+                       name varchar(200),
+                       author_id integer references authors(id)
+);
+
+insert into books (name, author_id) values ('Евгений Онегин', 1);
+insert into books (name, author_id) values ('Капитанская дочка', 1);
+insert into books (name, author_id) values ('Дубровский', 1);
+insert into books (name, author_id) values ('Мертвые души', 2);
+insert into books (name, author_id) values ('Вий', 2);
+create table orders (
+                        id serial primary key,
+                        active boolean default true,
+                        book_id integer references books(id),
+                        student_id integer references students(id)
+);
+
+insert into orders (book_id, student_id) values (1, 1);
+insert into orders (book_id, student_id) values (3, 1);
+insert into orders (book_id, student_id) values (5, 2);
+insert into orders (book_id, student_id) values (4, 1);
+insert into orders (book_id, student_id) values (2, 2);
+
+create view show_who_took_the_book as
+select s.name  from orders
+    join books b on orders.book_id = b.id
+    join authors a on a.id = b.author_id
+    join students s on orders.student_id = s.id
+where b.name = 'Капитанская дочка';
+
+select * from show_who_took_the_book
+
+
